32c7626b9abf2f6b2db1efcf344f70eb5a684861
rZn24h <razvanhertanu@gmail.com>
2025-09-18 17:12:02 +0300

    bug fix

--- src/app/rentals/[slug]/page.tsx
+++ src/app/rentals/[slug]/page.tsx
@@ -1,15 +1,15 @@
-import { doc, getDoc } from 'firebase/firestore';
-import { db } from '@/utils/firebase';
-import { notFound } from 'next/navigation';
-import RentalClient from '@/app/rentals/[slug]/RentalClient';
-import '@/app/cars/CarGallery.css';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/utils/firebase";
+import { notFound } from "next/navigation";
+import RentalClient from "@/app/rentals/[slug]/RentalClient";
+import "@/app/cars/CarGallery.css";
 
 export async function generateMetadata(props: { params: { slug: string } }) {
   const { slug } = props.params;
-  const docRef = doc(db, 'rentals', slug);
+  const docRef = doc(db, "rentals", slug);
   const docSnap = await getDoc(docRef);
   if (!docSnap.exists()) {
-    return { title: 'Anunț inexistent' };
+    return { title: "Anunț inexistent" };
   }
   const rental = docSnap.data();
   return { title: `${rental.marca} ${rental.model} - Închiriere` };
@@ -17,11 +17,14 @@ export async function generateMetadata(props: { params: { slug: string } }) {
 
 export default async function Page(props: { params: { slug: string } }) {
   const { slug } = props.params;
-  const docRef = doc(db, 'rentals', slug);
+  const docRef = doc(db, "rentals", slug);
   const docSnap = await getDoc(docRef);
   if (!docSnap.exists()) {
     notFound();
   }
-  const rental = { id: docSnap.id, ...docSnap.data() } as any;
+  const rental = {
+    id: docSnap.id,
+    ...JSON.parse(JSON.stringify(docSnap.data())),
+  } as any;
   return <RentalClient rental={rental} />;
-} 
+}
